65ec67dfba418ad0ff927d64cf7f15a811dd1298
twclark0 <tylerc644@gmail.com>
2019-09-18 12:17:27 -0600

    Should be done now, but probably not

---
+++ components/Footer/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const styles = {
+  position: 'absolute',
+  bottom: '0',
+  right: '25px',
+  fontSize: '30px',
+  color: '#f4592f'
+}
+
+const Footer = () => (
+  <div>
+    <p style={styles}>@iamtylerwclark</p>
+  </div>
+)
+
+export default Footer
